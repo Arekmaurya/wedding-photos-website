337283a1b34cb15ef637630267436b17396225bb
Harsh Maurya <harshmaurya82500@gmail.com>
2026-03-10 13:16:47 +0530

    Increase upload size limits to 100MB

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -121,3 +121,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Increase upload limits for large photos/videos (100MB)
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000
