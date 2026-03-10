c464f3dbf9eab084a8532e1139a08a6efa0794c7
Harsh Maurya <harshmaurya82500@gmail.com>
2026-03-10 13:52:32 +0530

    Bypass form validation for files and fix runtime.txt format

--- uploader/forms.py
+++ uploader/forms.py
@@ -1,12 +1,4 @@
 from django import forms
 
-class MultipleFileInput(forms.FileInput):
-    allow_multiple_selected = True
-
 class UploadFileForm(forms.Form):
     guest_name = forms.CharField(max_length=100, label="Your Name", required=True)
-    file = forms.FileField(
-        widget=MultipleFileInput(attrs={'multiple': True}), 
-        label='Select Photos & Videos',
-        required=False  # Handled manually in view
-    )
