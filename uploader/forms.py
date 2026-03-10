05e2ba0383fea173f6662c7b97f2200c0eea2a4a
Harsh Maurya <harshmaurya82500@gmail.com>
2026-03-10 12:54:17 +0530

    Enable multiple file selection in upload form

--- uploader/forms.py
+++ uploader/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     guest_name = forms.CharField(max_length=100, label="Your Name", required=True)
-    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected': True}), label='Select Photos & Videos')
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label='Select Photos & Videos')
